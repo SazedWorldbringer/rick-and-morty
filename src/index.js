b780fa0124443262b72ff13b4e2d401773506826
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-13 01:37:43 +0530

    refactor: style title, change single quotes to double

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import ReactDOM from 'react-dom/client'
-import List from './components/List';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom/client"
+import List from "./components/List";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 	return (
-		<div className='container'>
-			<h1>Rick and Morty</h1>
+		<div className="container">
+			<nav className="navbar stick-top navbar-light bg-dark">
+				<h1 className="ms-3 navbar-brand text-light">
+					Rick and Morty
+				</h1>
+			</nav>
 			<List />
 		</div>
 	)
 }
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
